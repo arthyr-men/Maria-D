2c9ad39a89a696cca14282941110530882b14177
Artur-2023 <123097639+arthyr-men@users.noreply.github.com>
2024-01-28 18:57:38 +0100

    44

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Сторінка відпочинку на морі завантажена!");
+function openModal(src) {
+    var modal = document.getElementById("myModal");
+    var modalImg = document.getElementById("img01");
+    var captionText = document.getElementById("caption");
+    var span = document.getElementsByClassName("close")[0];
+
+    modal.style.display = "block";
+    modalImg.src = src;
+    captionText.innerHTML = src.split('/').pop();
+
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+}
